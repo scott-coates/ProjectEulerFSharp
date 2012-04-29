2eb814a9b141b73f7564dad0908c983c835b6fc1
Scott Coates <devnull@localhost>
2012-04-28 21:36:09 -0700

    solved problem 1

---
+++ ProjectEuler/Problem1.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.net
+
+open System
+let rec GetMultiples n =
+    if n < 1000 then
+        let x = if n % 3 = 0 || n % 5 = 0 then n else 0
+        x + GetMultiples (n + 1)
+    else 0
+
+let x = GetMultiples 1
+Console.Write(x)
+
+ignore(Console.Read())
