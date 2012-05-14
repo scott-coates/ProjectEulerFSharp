bf330204d5fdde2269f7fce8ae7afca0e5ae9078
Scott Coates <devnull@localhost>
2012-05-14 13:14:55 -0700

    attempting 3

---
+++ ProjectEuler/Problem3.fs
@@ -0,0 +1,29 @@
+﻿module Problem3
+open System
+
+let primesUnder (max:int64) = 
+    let mutable largest = 0L
+    let isFactorOfMax (n:int64) =
+        max % n = 0L
+    for n in List.filter isFactorOfMax [1L .. max] do
+        let factorialsOfN = 
+            [   
+                for i in 1L .. n do
+                    if n % i = 0L then
+                        yield i
+                    else
+                        ignore() 
+            ]
+
+        if (List.length factorialsOfN) = 2 then
+            if n > largest then
+                largest <- n
+        else
+//                List.length factorialsOfN |> printfn "Length: %d"
+            ignore()
+
+    largest
+
+let  x = primesUnder 600851475143L
+Console.Write(x)
+ignore(Console.Read())
