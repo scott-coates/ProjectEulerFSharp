a802d6d3cf0b9929665ee05e422249fafb668bf0
Scott Coates <devnull@localhost>
2012-07-04 13:47:06 -0700

    testing idea

--- ProjectEuler/Problem3.fs
+++ ProjectEuler/Problem3.fs
@@ -2,27 +2,20 @@
 open System
 
 let primesUnder (max:int64) = 
-    let mutable largest = 0L
     let isFactorOfMax (n:int64) =
         max % n = 0L
-    for n in List.filter isFactorOfMax [1L .. max] do
-        let factorialsOfN = 
-            [   
-                for i in 1L .. n do
-                    if n % i = 0L then
-                        yield i
-                    else
-                        ignore() 
-            ]
+    
 
-        if (List.length factorialsOfN) = 2 then
-            if n > largest then
-                largest <- n
-        else
-//                List.length factorialsOfN |> printfn "Length: %d"
-            ignore()
+    let rec loopThroughNumbers n =
+        if isFactorOfMax n then
+                n
+        else loopThroughNumbers (n - 1L)
 
-    largest
+    let starting = int64(Math.Floor(Math.Sqrt(float(max))))
+//    let starting = int64(Math.Floor( float(max) /  2.0))
+
+
+    loopThroughNumbers starting
 
 let  x = primesUnder 600851475143L
 Console.Write(x)
